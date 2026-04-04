68fb7f8cb204c3f5db72d4c8a9e8aec26e0ea2cd
Bhumika-SN <bhumikasnn.dev@gmail.com>
2026-04-04 09:50:45 +0530

    Array & Multidimensional array

---
+++ JAVA/src/Array.java
@@ -0,0 +1,10 @@
+public class Array{
+
+    public static void main(String [] args){
+        int num[] = new int[6] ;
+
+        for(int i =0; i<6; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
